523e9fbe86eb3a0abfbe2e0d84f2e5b53e966662
Paulojob69 <paulo.senger.filho@escola.pr.gov.br>
2024-09-23 15:32:17 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+
+
+function setup() { 
+ createCanvas(400, 400);
+ background("pink")
+ 
+  
+}
+function draw() {
+  
+    fill("white")
+  
+  ellipse(200, 310, 170, 170);
+   
+  ellipse(200, 250, 130, 120);
+  
+  ellipse(200, 180, 100, 100);
+
+  fill("black")
+  
+  circle(180, 175, 20); 
+  
+  circle(220, 175, 20);
+  
+  fill("orange")
+  
+  triangle(200, 190, 200, 204, 212, 202);
+  
+  line(190, 216, 210, 217);
+  
+  fill("black")
+  
+  ellipse(200, 285, 15, 15);
+  
+  ellipse(200, 265, 15, 15);
+  
+  ellipse(200, 245, 15, 15);
+  
+  line(335, 280, 264, 240);
+  
+  line(135, 250, 70, 290);
+  
+    
+  
+  
+  
+  
+  
+  
+  
+  
+}
